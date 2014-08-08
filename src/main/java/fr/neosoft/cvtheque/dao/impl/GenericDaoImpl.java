ef384b571c19df82ceaa980fc6f1a2b886c2e14d
Adrien Cambillau <a.cambillau@orange.fr>
2014-08-08 16:26:30 +0200

    Correction

--- src/main/java/fr/neosoft/cvtheque/dao/impl/GenericDaoImpl.java
+++ src/main/java/fr/neosoft/cvtheque/dao/impl/GenericDaoImpl.java
@@ -18,7 +18,7 @@ public abstract class GenericDaoImpl<T> implements GenericDao<T> {
 	public GenericDaoImpl(){
 		Type t = getClass().getGenericSuperclass();
 		ParameterizedType pt = (ParameterizedType) t;
-		type = (Class) pt.getActualTypeArguments()[0];
+		type = (Class<T>) pt.getActualTypeArguments()[0];
 	}
 
 	public T create(T t) {
